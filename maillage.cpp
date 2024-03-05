52bf027aaabd98ab2fd396b44025c6004f904ae0
RaphaelB-Ensta <bernasraphael@gmail.com>
2024-03-05 11:01:58 +0100

    Update supprimerpoint

--- maillage.cpp
+++ maillage.cpp
@@ -496,8 +496,13 @@ void Front::supprimerSegment(const Segment* segment) {    // Supprime une arête
 void Front::ajouterPoint(const Sommet& point) {     // Ajoute un point à la liste des points
     points.push_back(point);
 }
-void supprimerPoint(const Sommet& point) { // Supprime un point de notre liste
-    
+void Front::supprimerPoint(const Sommet& point) {
+    for (auto it = points.begin(); it != points.end(); ++it) {
+        if (*it == point) {
+            points.erase(it);
+            break; // Sortir de la boucle une fois que le point est trouvé et supprimé
+        }
+    }
 }
 Triangle Front::genererTriangle() { 
     // générer le 3e sommet du triangle équilatéral
