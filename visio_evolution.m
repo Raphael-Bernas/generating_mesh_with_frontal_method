f4ab13709ecfb0e46ed330a3f48eb081a8a3dd7a
louispaill <louis.pailloncy@ensta-paris.fr>
2024-03-10 15:20:34 +0100

    modifs

--- visio_evolution.m
+++ visio_evolution.m
@@ -4,10 +4,10 @@ clear all ;
 maillage_evol = dlmread('historique_M.txt') ;
 front_evol = dlmread('historique_F.txt') ;
 
-it=1
-
+itm=1
+itf=1
 % Parcourir la matrice maillage_evol
-for i = it:size(data, 1)
+while i <=size(data, 1)
     sommets = [];
     triangles=[];
     % Vérifier si la première lettre de la première colonne est "S"
@@ -17,9 +17,9 @@ for i = it:size(data, 1)
     elseif maillage_evol(i, 1) == 'T'
         triangles = [triangles; data(i, 2:4)];
     elif maillage_evol(i, 1) == 'F'
-        it=i+1; %pour reprendre la prochaine fois à partir du 'F'
+        itm=i+1; %pour reprendre la prochaine fois à partir du 'F'
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        %%%Visualisation
+        %%%Visualisation du maillage
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         triplot(triangles(:, [1 2 3]), sommets(:, 1), sommets(:, 2), 'k') ;
         scatter(sommets(:, 1), sommets(:, 2), 'r') ;
@@ -29,23 +29,23 @@ for i = it:size(data, 1)
         title('Maillage') ;
         axis equal ;
         axis tight;
-        break;
+        % Parcourir la matrice front_evol
+        for i = it:size(front_evol, 1)
+             sommets = [];
+            % Vérifier si la première lettre de la première colonne est "F"
+            if front_evol(i, 1) == 'F'
+                itf=i+1; %pour reprendre la prochaine fois à partir du 'F'
+                %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+                %%%Visualisation du front
+                %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+                hold on;
+                scatter(sommets(:, 1), sommets(:, 2), 'b');
+                pause(1);
+                else
+                sommets = [sommets; data(i, 2:3)];
+            end
+        end
+
     end
 end
 
-% Parcourir la matrice front_evol
-it=0
-for i = it:size(front_evol, 1)
-    sommets = [];
-    % Vérifier si la première lettre de la première colonne est "F"
-    if front_evol(i, 1) == 'F'
-        it=i+1; %pour reprendre la prochaine fois à partir du 'F'
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        %%%Visualisation
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        hold on;
-        scatter(sommets(:, 1), sommets(:, 2), 'b');
-    else
-        sommets = [sommets; data(i, 2:3)];
-    end
-end
